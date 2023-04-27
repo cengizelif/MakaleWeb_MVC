18f64bfcbb96a32d5cd58d42322949ef16714c8e
cengizelif <cenelif@gmail.com>
2023-04-27 14:58:36 +0300

    index

--- MakaleBLL/KategoriYonet.cs
+++ MakaleBLL/KategoriYonet.cs
@@ -15,5 +15,11 @@ namespace MakaleBLL
         {
             return rep_kat.Liste();
         }
+
+        public Kategori KategoriBul(int id)
+        {
+            return rep_kat.Find(x=>x.Id == id); 
+        }
+
     }
 }
--- MakaleWeb_MVC/Controllers/HomeController.cs
+++ MakaleWeb_MVC/Controllers/HomeController.cs
@@ -10,24 +10,54 @@ namespace MakaleWeb_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        MakaleYonet my = new MakaleYonet();
+        KategoriYonet ky = new KategoriYonet();
         public ActionResult Index()
         {
             // Test test = new Test();
             //  test.EkleTest();
             // test.UpdateTest();
             // test.DeleteTest();
-            // test.YorumTest();
-
-            MakaleYonet my=new MakaleYonet();
+            // test.YorumTest();         
           
             return View(my.Listele());
         }
 
-        public PartialViewResult kategoriPartial()
+        public ActionResult Kategori(int? id)
         {
-            KategoriYonet ky = new KategoriYonet();
-            List<Kategori> liste = ky.Listele();
-            return PartialView("_PartialPagekat2", liste);
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Kategori kat = ky.KategoriBul(id.Value);
+            
+            return View("Index",kat.Makaleler);
         }
+
+        public ActionResult EnBegenilenler()
+        {
+            return View("Index",my.Listele().OrderByDescending(x=>x.BegeniSayisi).ToList());
+        }
+
+        public ActionResult SonYazilanlar()
+        {
+            return View("Index", my.Listele().OrderByDescending(x => x.DegistirmeTarihi).ToList());
+        }
+
+        public ActionResult Hakkımızda()
+        {
+            return View();
+        }
+
+
+
+
+        //public PartialViewResult kategoriPartial()
+        //{
+        //    KategoriYonet ky = new KategoriYonet();
+        //    List<Kategori> liste = ky.Listele();
+        //    return PartialView("_PartialPagekat2", liste);
+        //}
     }
 }
