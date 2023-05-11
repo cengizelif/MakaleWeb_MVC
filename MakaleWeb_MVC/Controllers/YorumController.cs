850e4a1da1f2b993f6de31565d527497c1c17689
cengizelif <cenelif@gmail.com>
2023-05-11 14:58:33 +0300

    yorum işlemleri

--- MakaleWeb_MVC/Controllers/YorumController.cs
+++ MakaleWeb_MVC/Controllers/YorumController.cs
@@ -30,5 +30,78 @@ namespace MakaleWeb_MVC.Controllers
 
             return PartialView("_PartialPageYorumlar",makale.Yorumlar);
         }
+
+        public ActionResult YorumGuncelle(int? id,string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            YorumYonet yy = new YorumYonet();
+            Yorum yorum=yy.YorumBul(id.Value);
+
+            if (yorum == null) 
+            {
+                return HttpNotFound();
+            }
+
+            yorum.Text= text;   
+
+            if(yy.YorumUpdate(yorum)>0)
+            {
+                return Json(new { hata = false }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { hata = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult YorumSil(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            YorumYonet yy = new YorumYonet();
+            Yorum yorum = yy.YorumBul(id.Value);
+
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (yy.yorumSil(yorum) > 0)
+            {
+                return Json(new { hata = false }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { hata = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult YorumEkle(Yorum nesne,int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            YorumYonet yy = new YorumYonet();
+            Yorum yorum = yy.YorumBul(id.Value);
+
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (yy.yorumEkle(yorum) > 0)
+            {
+                return Json(new { hata = false }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { hata = true }, JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
