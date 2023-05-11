09765740f365fa1260bfa251efbe313d98ba24e3
cengizelif <cenelif@gmail.com>
2023-05-11 10:34:57 +0300

    yorum eklendi

---
+++ MakaleWeb_MVC/Controllers/YorumController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MakaleBLL;
+using MakaleEntities;
+
+namespace MakaleWeb_MVC.Controllers
+{
+    public class YorumController : Controller
+    {
+        // GET: Yorum
+        public ActionResult YorumGoster(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MakaleYonet my = new MakaleYonet();
+
+            Makale makale=my.MakaleBul(id.Value);
+
+            if (makale == null) 
+            { 
+               return HttpNotFound();   
+            }
+
+            return PartialView("_PartialPageYorumlar",makale.Yorumlar);
+        }
+    }
+}
--- MakaleWeb_MVC/Scripts/CommentScript.js
+++ MakaleWeb_MVC/Scripts/CommentScript.js
@@ -3,9 +3,10 @@
 
     $('#modal1').on('show.bs.modal', function (e) {
 
+       var btn = $(e.relatedTarget);
+       var mid = btn.data("makaleid");
 
-
-
+        $("#modal1_body").load("/Yorum/YorumGoster/" + mid);
 
     })
 }
